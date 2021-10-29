cdf3de66d6728324afff30f01f5c9e3690817351
c0034805 <c0034805@newcastle.ac.uk>
2021-10-29 19:38:28 +0100

    implement limited user logins

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ import pyotp
 import logging
 from functools import wraps
 
-from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask import Blueprint, render_template, flash, redirect, url_for, request, session
 from flask_login import current_user, login_user, logout_user
 
 from werkzeug.security import check_password_hash
@@ -58,18 +58,26 @@ def register():
 # view user login
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
+    # if session attribute logins does not exist create attribute logins
+    if not session.get('logins'):
+        session['logins'] = 0
+    # if login attempts is 3 or more create an error message
+    elif session.get('logins') >= 3:
+        flash('Number of incorrect logins exceeded')
+
     form = LoginForm()
 
     if form.validate_on_submit():
+        # increase login attempts by 1
+        session['logins'] += 1
 
         user = User.query.filter_by(email=form.email.data).first()
 
-        if not user or not check_password_hash(user.password, form.password.data):
-            flash('Please check your login details and try again')
+        if user and check_password_hash(user.password, form.password.data) \
+                and pyotp.TOTP(user.pin_key).verify(form.pin.data):
 
-            return render_template('login.html', form=form)
-
-        if pyotp.TOTP(user.pin_key).verify(form.pin.data):
+            # if user is verified reset login attempts to 0
+            session['logins'] = 0
 
             login_user(user)
 
@@ -78,9 +86,19 @@ def login():
             db.session.add(user)
             db.session.commit()
 
-            return profile()
+            # direct to role appropriate page
+            if current_user.role == 'admin':
+                return redirect(url_for('admin.admin'))
+            else:
+                return redirect(url_for('users.profile'))
+
         else:
-            flash("You have supplied an invalid 2FA token!", "danger")
+            if session['logins'] == 3:
+                flash('Number of incorrect logins exceeded')
+            elif session['logins'] == 2:
+                flash('Please check your login details and try again. 1 login attempt remaining')
+            else:
+                flash('Please check your login details and try again. 2 login attempts remaining')
 
     return render_template('login.html', form=form)
 
